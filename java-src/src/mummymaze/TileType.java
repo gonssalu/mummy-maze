294b33bb8a368275a37162623d1be500fc4d597c
tgn <trwygon@outlook.pt>
2022-04-28 16:10:16 +0100

    Tile Type

---
+++ java-src/src/mummymaze/TileType.java
@@ -0,0 +1,53 @@
+package mummymaze;
+
+public enum TileType {
+    AGENT('H'),
+    V_WALL('|'),
+    H_WALL('-'),
+    EMPTY('.'),
+    OPEN(' '),
+    EXIT('S'),
+    WHITE_MUMMY('M'),
+    RED_MUMMY('V'),
+    TRAP('A'),
+    SCORPION('E'),
+    KEY('C'),
+    H_DOOR_OPEN('_'),
+    H_DOOR_CLOSED('='),
+    V_DOOR_OPEN('"'),
+    V_DOOR_CLOSED('(');
+
+    private char identifier;
+
+    TileType(char identifier){
+        this.identifier = identifier;
+    }
+
+    public char getIdentifier(){ return identifier; }
+
+    public static TileType getTileType(char identifier){
+        for(TileType tileType : TileType.values()){
+            if(tileType.getIdentifier()==identifier)
+                return tileType;
+        }
+        throw new IllegalArgumentException("No tile type was found with that identifier.");
+    }
+
+    private static boolean blocksPassage(TileType tileType){
+        return (
+            tileType==TileType.WHITE_MUMMY || tileType==TileType.RED_MUMMY ||
+                    tileType==TileType.TRAP || tileType==TileType.SCORPION );
+    }
+
+    public static boolean blocksVerticalPassage(TileType tileType){
+        return (blocksPassage(tileType) || (
+                tileType==TileType.H_WALL || tileType==TileType.H_DOOR_CLOSED
+        ));
+    }
+
+    public static boolean blocksHorizontalPassage(TileType tileType){
+        return (blocksPassage(tileType) || (
+                tileType==TileType.V_WALL || tileType==TileType.V_DOOR_CLOSED
+        ));
+    }
+}
