192175389ddf1abfe32a16a8414cd3ec6feb4edb
tgn <trwygon@outlook.pt>
2022-05-30 13:32:40 +0100

    [0.6] Action Stay

---
+++ java-src/src/mummymaze/ActionStay.java
@@ -0,0 +1,20 @@
+package mummymaze;
+
+import agent.Action;
+
+public class ActionStay extends Action<MummyMazeState> {
+
+    public ActionStay() {
+        super(1);
+    }
+
+    @Override
+    public void execute(MummyMazeState state) {
+        state.setAction(this);
+    }
+
+    @Override
+    public boolean isValid(MummyMazeState state) {
+        return true;
+    }
+}
